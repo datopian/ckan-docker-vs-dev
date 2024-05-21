fecc5b6f9ce564beb01aab4ef83a0d393a0460ab
Michael Polidori <contact@michaelpolidori.com>
2024-05-21 13:17:01 -0400

    Fix integration-tests (attempt v2)

--- integration-tests/test.js
+++ integration-tests/test.js
@@ -18,6 +18,20 @@ console.log(`Running tests with options: ${JSON.stringify(assets.options, null, 
 
 cypress
   .run(assets.options)
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => assets.cleanUp());
+  .then(r => {
+    console.log(r)
+    assets.cleanUp()
+
+    // if the cypress tests fail to run, log the error
+    if (r.failures) {
+      console.error('Could not execute cypress tests')
+      console.error(r.message)
+      process.exit(1)
+    }
+    // return exit code of 1 if any tests fail
+    r.totalFailed ? process.exit(1) : process.exit(0)
+  })
+  .catch(e => {
+    console.error(e.message)
+    process.exit(1)
+  });
