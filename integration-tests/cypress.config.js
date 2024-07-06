3f3a46bd5f382aee656ff3dd62a8078b6c5c6f93
Michael Polidori <contact@michaelpolidori.com>
2024-07-06 16:01:29 -0400

    Add report suffix to cypress.config.js

--- integration-tests/cypress.config.js
+++ integration-tests/cypress.config.js
@@ -7,12 +7,13 @@ export default defineConfig({
   env: {
     FRONTEND_URL: "",
     API_KEY:
-      "CKAN_API_TOKEN",
+      "CKAN_API_KEY",
     CKAN_USERNAME: "ckan_admin",
     CKAN_PASSWORD: "test1234",
     ORG_NAME_SUFFIX: "_organization_test",
     GROUP_NAME_SUFFIX: "_group_test",
     DATASET_NAME_SUFFIX: "_dataset_test",
+    REPORT_NAME_SUFFIX: "_report_test",
   },
   e2e: {
     baseUrl: "http://ckan-dev:5000",
